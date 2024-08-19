19985a5798e2562f51de663fecd10b684025b026
VendettaReborn <vendettareborn@proton.me>
2024-08-19 15:08:47 +0800

    fix windows error

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,31 +1,28 @@
 use super::Opt;
+use net_route::Route;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
-pub fn build_routes(gateway: IpAddr, opt: &Opt) -> Vec<Route> {
+pub fn build_routes(opt: &Opt) -> Vec<Route> {
     let mut routes = Vec::with_capacity(2);
-    if opt.gateway_ipv4.is_some() {
+    if let Some(gateway_ipv4) = opt.gateway_ipv4 {
         let mut r1 = Route::new(Ipv4Addr::UNSPECIFIED.into(), 1)
-            .with_gateway(gateway)
+            .with_gateway(IpAddr::V4(gateway_ipv4))
             .with_metric(0);
         if let Some(luid) = opt.luid {
             r1 = r1.with_luid(luid);
         }
-        if let Some(if_index) = opt.if_index {
-            r1 = r1.with_ifindex(if_index);
-        }
+        r1 = r1.with_ifindex(opt.if_index);
         routes.push(r1);
     }
 
-    if opt.gateway_ipv6.is_some() {
+    if let Some(gateway_ipv6) = opt.gateway_ipv6 {
         let mut r2 = Route::new(Ipv6Addr::UNSPECIFIED.into(), 1)
-            .with_gateway(gateway)
+            .with_gateway(IpAddr::V6(gateway_ipv6))
             .with_metric(0);
         if let Some(luid) = opt.luid {
             r2 = r2.with_luid(luid);
         }
-        if let Some(if_index) = opt.if_index {
-            r2 = r2.with_ifindex(if_index);
-        }
+        r2 = r2.with_ifindex(opt.if_index);
         routes.push(r2);
     }
 
